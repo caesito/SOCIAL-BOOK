f076032afbba4ad8b2220ab2db4e488d29a1bdb6
Caesar Tiago <71521248+caesito@users.noreply.github.com>
2022-07-28 14:25:40 -0300

    feat: customization model Users, extends AbstractUser

--- authentication/models.py
+++ authentication/models.py
@@ -1,2 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Users(AbstractUser):
+    address=models.CharField(max_length=100)
+    city=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
